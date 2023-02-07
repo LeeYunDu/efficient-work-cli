5e97aefcf24f04d1c67e59b7a99af8cb2d821950
liyundu <492107910@qq.com>
2023-02-07 18:03:36 +0800

    feat 他妈的编译ts转js 成功咯

--- bin/efwork.ts
+++ bin/efwork.ts
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 require('ts-node/register');
-// import { program } from 'commander'
-const { program } = require('commander')
-const { genStart } = require('./efwork-g.ts')
+import { program } from 'commander'
+// const { program } = require('commander')
+// const { genStart } = require('./efwork-g.ts')
 
+// import {program}
+import { genStart } from './efwork-g'
 
 // genrator
 program
   .command('g <type> [path]')
   .description('生成type对应到到模版代码到指定目录')
   .option('-f --force', '如果存在输入的项目目录，强制删除项目目录')
-  .action((type: any, path: string, cmd: any) => {
+  .action((type, path, cmd) => {
     genStart({ type, path, force: cmd.force ?? false })
   })
 
--- dist/bin/efwork.js
+++ dist/bin/efwork.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 require('ts-node/register');
-// import { program } from 'commander'
-const { program } = require('commander');
-const { genStart } = require('./efwork-g.ts');
+const commander_1 = require("commander");
+// const { program } = require('commander')
+// const { genStart } = require('./efwork-g.ts')
+// import {program}
+const efwork_g_1 = require("./efwork-g");
 // genrator
-program
+commander_1.program
     .command('g <type> [path]')
     .description('生成type对应到到模版代码到指定目录')
     .option('-f --force', '如果存在输入的项目目录，强制删除项目目录')
     .action((type, path, cmd) => {
     var _a;
-    genStart({ type, path, force: (_a = cmd.force) !== null && _a !== void 0 ? _a : false });
+    (0, efwork_g_1.genStart)({ type, path, force: (_a = cmd.force) !== null && _a !== void 0 ? _a : false });
 });
-program.parse(process.argv);
+commander_1.program.parse(process.argv);
