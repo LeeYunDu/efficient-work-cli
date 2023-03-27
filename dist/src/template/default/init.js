6d9950a555fbd43473e5b2cc4c3d009a360f0ac9
liyundu <492107910@qq.com>
2023-03-27 09:40:45 +0800

    feat: 生成默认表格组件

--- dist/src/template/default/init.js
+++ dist/src/template/default/init.js
@@ -8,22 +8,41 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useDefaultFile = void 0;
 const utils_1 = require("../../utils");
 const ast_1 = require("../../utils/ast");
+const prompts_1 = __importDefault(require("prompts"));
+const COMPONENT_PATH_MAP = {
+    default: ''
+};
 function useDefaultFile(path) {
     return __awaiter(this, void 0, void 0, function* () {
+        let { type } = yield (0, prompts_1.default)(promptsOptions);
         // 创建该目录下所需要的模板文件
         let modelPath = `${process.cwd()}/${path}`;
         let hasDir = yield (0, utils_1.mkdir)(modelPath);
-        let file = '/default.vue';
+        let file = `/${type}.vue`;
         (0, utils_1.createFile)({ filePath: `${modelPath}${file}` });
         const rootDir = yield (0, utils_1.getSourcePath)();
-        const sourcePath = `${rootDir}/src/template/default/template/vue.vue`;
+        const sourcePath = `${rootDir}/src/template/default/template/${type}.vue`;
         let templateResult;
         let componentsAst = new ast_1.Ast(sourcePath, { parseOptions: { language: 'vue' } });
         componentsAst.writeFile(`${modelPath}${file}`);
     });
 }
 exports.useDefaultFile = useDefaultFile;
+const promptsOptions = [
+    {
+        type: 'select',
+        name: 'type',
+        message: '选择生成模板',
+        choices: [
+            { title: 'setup+ts的单组件文件', value: 'vue' },
+            { title: '表格', value: 'table' }
+        ]
+    },
+];
--- src/template/default/init.ts
+++ src/template/default/init.ts
@@ -1,16 +1,36 @@
 import { createFile, getSourcePath, mkdir } from "../../utils"
 import { Ast } from "../../utils/ast"
+import prompts from 'prompts'
 
 
+
+const COMPONENT_PATH_MAP = {
+  default: ''
+}
 export async function useDefaultFile (path: string) {
+  let { type } = await prompts(promptsOptions)
+
   // 创建该目录下所需要的模板文件
   let modelPath = `${process.cwd()}/${path}`
   let hasDir = await mkdir(modelPath)
-  let file = '/default.vue'
+  let file = `/${type}.vue`
   createFile({ filePath: `${modelPath}${file}` })
   const rootDir = await getSourcePath()
-  const sourcePath = `${rootDir}/src/template/default/template/vue.vue`
+  const sourcePath = `${rootDir}/src/template/default/template/${type}.vue`
   let templateResult: any
   let componentsAst = new Ast(sourcePath, { parseOptions: { language: 'vue' } })
   componentsAst.writeFile(`${modelPath}${file}`)
 }
+
+
+const promptsOptions: any = [
+  {
+    type: 'select',//单选
+    name: 'type',
+    message: '选择生成模板',
+    choices: [
+      { title: 'setup+ts的单组件文件', value: 'vue' },
+      { title: '表格', value: 'table' }
+    ]
+  },
+]
