f273d64767261e230eba72c0c7d71160b9902b0c
liyundu <492107910@qq.com>
2023-08-04 11:01:35 +0800

    feat: no data tip component

---
+++ src/template/model/other/item.noData.vue
@@ -0,0 +1,60 @@
+
+<template>
+  <div v-if="show" class="no-data">
+    <img :src="url" alt="">
+    <div class="tip">{{ tip }}</div>
+  </div>
+  <template v-if="!show">
+    <slot></slot>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+let props = defineProps({
+  data:{
+    type:Array||Object,
+    default:()=>({})
+  },
+  tip:{
+    type:String,
+    default:'暂无数据'
+  },
+  url:{
+    type:String,
+    default:'static/images/common/no-data.png'
+  }
+
+})
+
+const show = computed(()=>{
+  try {
+    // 数组
+    if(Array.isArray(props.data)){
+      return props.data.length===0
+    }else if('dimensions' in props.data && 'data' in props.data){
+      // 图表数据
+      let length = props.data?.data.length??0
+      return !(length>0)
+    }else{
+      // 对象
+      return JSON.stringify(props.data)==='{}'
+    }
+  } catch (error) {
+    return false
+  }
+
+})
+
+</script>
+
+<style lang="scss" scoped>
+.no-data{
+  text-align: center;
+  img{
+    width: 202px;
+    height: 180px;
+  }
+}
+</style>
